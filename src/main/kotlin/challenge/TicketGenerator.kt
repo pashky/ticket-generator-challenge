cfc7449bbbc8c791de4dd978ef3ecdafdac82b12
Pavel Kurnosov <pashky@gmail.com>
2019-09-12 22:30:16 +0100

    Renames and clean-ups

--- src/main/kotlin/challenge/TicketGenerator.kt
+++ src/main/kotlin/challenge/TicketGenerator.kt
@@ -2,9 +2,9 @@ package challenge
 
 import kotlin.random.Random
 
-typealias StripRow = List<Int?>
+typealias TicketRow = List<Int?>
 
-class Strip(val rows: List<StripRow>) {
+class Ticket(val rows: List<TicketRow>) {
     private fun Int.format(s: String) = s.format(this)
     override fun toString(): String = rows.joinToString("\n") { row ->
         row.joinToString(" ") { it?.format("%-2d") ?: "  " }
@@ -12,12 +12,13 @@ class Strip(val rows: List<StripRow>) {
 }
 
 class TicketGenerator(val random: Random = Random) {
-    inner class StripBuilder {
+    inner class TicketBuilder {
         private val columns: Array<MutableList<Int?>> = Array(9) { mutableListOf<Int?>() }
         private fun total() = columns.sumBy { it.size }
 
         fun isFull() = total() == 15
         fun isColFull(col: Int) = columns[col].size == 3
+
         fun canAddTo(col: Int) = columns[col].size < 3 && !isFull()
         fun addTo(col: Int, number: Int) = columns[col].add(number)
 
@@ -26,7 +27,7 @@ class TicketGenerator(val random: Random = Random) {
 
         fun removeAt(col: Int): Int = columns[col].removeAt(random.nextInt(columns[col].size)) ?: 0
 
-        fun build(): Strip {
+        fun build(): Ticket {
             columns.forEach { it.sortWith(nullsLast()) }
 
             colsOfSize { it < 3 }.shuffled(random).asSequence().take(4).forEach { columns[it].add(0, null) }
@@ -34,46 +35,45 @@ class TicketGenerator(val random: Random = Random) {
             colsOfSize { it == 2 }.shuffled(random).asSequence().take(toShift).forEach { columns[it].add(1, null) }
 
             columns.forEach { while (it.size < 3) it.add(null) }
-            return Strip((0..2).map { rowNum -> columns.map { it[rowNum] } })
+            return Ticket((0..2).map { rowNum -> columns.map { it[rowNum] } })
         }
     }
 
     private fun <T> List<T>.getRandom(random: Random): T = get(random.nextInt(size))
 
-    fun generate(equalSplit: Boolean = false): List<Strip> {
+    fun generate(equalSplit: Boolean = false): List<Ticket> {
         val columns =
                 if (equalSplit) (1..90).chunked(10)
                 else listOf(1..9, 10..19, 20..29, 30..39, 40..49, 50..59, 60..69, 70..79, 80..90)
 
         val available = columns.map { c -> c.shuffled(random).toMutableList() }.withIndex()
-        val stripBuilders = List(6) { StripBuilder() }
+        val ticketBuilders = List(6) { TicketBuilder() }
 
-        stripBuilders.forEach { strip ->
-            available.forEach { strip.addTo(it.index, it.value.removeAt(0)) }
+        ticketBuilders.forEach { ticket ->
+            available.forEach { ticket.addTo(it.index, it.value.removeAt(0)) }
         }
 
         do {
-            val haveLeftToPlace = available.map { currentCol ->
-                if (currentCol.value.isNotEmpty()) {
-                    val nonFull = stripBuilders.filter { it.canAddTo(currentCol.index) }
+            val leftToPlace = available.map { current ->
+                if (current.value.isNotEmpty()) {
+                    val nonFull = ticketBuilders.filter { it.canAddTo(current.index) }
                     if (nonFull.isNotEmpty()) {
-                        nonFull.getRandom(random).addTo(currentCol.index, currentCol.value.removeAt(0))
+                        nonFull.getRandom(random).addTo(current.index, current.value.removeAt(0))
                     } else {
-                        val stripWithFullCol = stripBuilders.filterNot { it.isFull() }.getRandom(random)
-                        val stripFull = stripBuilders.filterNot { it.isColFull(currentCol.index) }.getRandom(random)
-                        val colToMove =
-                                (stripFull.colsOfSize { it > 1 } intersect stripWithFullCol.colsOfSize { it < 3 }).toList().getRandom(random)
-                        stripWithFullCol.addTo(colToMove, stripFull.removeAt(colToMove))
-                        stripFull.addTo(currentCol.index, currentCol.value.removeAt(0))
+                        val ticketWithFullCol = ticketBuilders.filterNot { it.isFull() }.getRandom(random)
+                        val ticketFull = ticketBuilders.filterNot { it.isColFull(current.index) }.getRandom(random)
+                        val columnToMove =
+                                (ticketFull.colsOfSize { it > 1 } intersect ticketWithFullCol.colsOfSize { it < 3 }).toList().getRandom(random)
+                        ticketWithFullCol.addTo(columnToMove, ticketFull.removeAt(columnToMove))
+                        ticketFull.addTo(current.index, current.value.removeAt(0))
                     }
                 }
-                currentCol.value.size
+                current.value.size
             }.sum()
-        } while (haveLeftToPlace > 0)
+        } while (leftToPlace > 0)
 
-        return stripBuilders.map { it.build() }
+        return ticketBuilders.map { it.build() }
     }
-
 }
 
 fun main() {
--- src/test/kotlin/challenge/TicketGeneratorTest.kt
+++ src/test/kotlin/challenge/TicketGeneratorTest.kt
@@ -6,17 +6,17 @@ import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class TicketGeneratorTest {
-    private fun Strip.isValid() = rows.all { row -> row.count { it != null } == 5 }
+    private fun Ticket.isValid() = rows.all { row -> row.count { it != null } == 5 }
             && (0..8).all { x -> (0..2).count { y -> rows[y][x] != null } in 1..3 }
 
-    private fun Strip.allNumbers() = rows.asSequence().flatMap { row -> row.asSequence().filterNotNull() }.toList()
+    private fun Ticket.allNumbers() = rows.asSequence().flatMap { row -> row.asSequence().filterNotNull() }.toList()
 
     @Test fun testGeneratedTicketsAreValid() {
         val random = Random
         for (i in 1..100000) {
-            val strips = TicketGenerator(random).generate()
-            assertTrue(strips.all { strip -> strip.isValid() })
-            assertEquals((1..90).toList(), strips.flatMap { strip -> strip.allNumbers() }.sorted())
+            val strip = TicketGenerator(random).generate()
+            assertTrue(strip.all { it.isValid() })
+            assertEquals((1..90).toList(), strip.flatMap { it.allNumbers() }.sorted())
         }
     }
 }
